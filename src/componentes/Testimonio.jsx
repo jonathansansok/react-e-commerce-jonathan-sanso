c6350f7837b44bdded4051660e040104c8321d3f
sansoj <jonasans2@live.com.ar>
2022-05-26 22:34:44 -0300

    first commit

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+export function Testimonio(props){
+    return (
+        <div className= 'contenedor-testimonio'>
+            <img
+             className= 'imagen-testimonio' 
+            src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+            alt= {props.imagen} />
+            <div className= 'contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'><strong>{props.nombre} en {props.pais}</strong></p>
+                <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+                <p className='texto-testimonio'>"{props.testimonio}"</p>
+            </div>
+
+        </div>
+    );
+}
+
+ export default Testimonio; 
