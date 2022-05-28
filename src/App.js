030d43f9e7940d3c6836a5d3ada9c6c64b75fc53
sansoj <jonasans2@live.com.ar>
2022-05-28 15:54:32 -0300

    js

--- src/App.js
+++ src/App.js
@@ -21,31 +21,72 @@ function App() {
             <h1>The adventure is waiting for you</h1>
             <button className="hero-button"><a href="App.js">Join us</a></button>
           </article>
-
-        <h2>Elige tus próximos viajes...</h2>
-        <Testimonio 
-        nombre='Shawn Wang'
-        pais='Singapur'
-        imagen='shawn'
-        cargo='Ingeniero de software'
-        empresa='Amazon'
-        testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
-        <Testimonio 
-        nombre='Sarah Chima'
-        pais='Nigeria'
-        imagen='sarah'
-        cargo='Ingeniero de software'
-        empresa='ChatDesk'
-        testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
-        <Testimonio 
-        nombre='Emma Bostian'
-        pais='Suecia'
-        imagen='emma'
-        cargo='Ingeniera de software'
-        empresa='Spotify'
-        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
-
-        
+            <h2>Elige tus próximos viajes...</h2>
+            <article className="okey">
+            <Testimonio 
+            nombre='Sky Ocean'
+            pais='tailandia'
+            imagen='tailandia'
+            cargo='disfruta el mar'
+            empresa='Amazon'
+            testimonio='Da miedo cambiar de carrera. Solo gané la ' />
+            <Testimonio 
+            nombre='Sarah Chima'
+            pais='Nigeria'
+            imagen='costarica'
+            cargo='Ingeniero de software'
+            empresa='ChatDesk'
+            testimonio='freeCodeCamp fue la puerta de entra' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+            <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='aspen'
+            cargo='Ingeniera de software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScr' />
+      </article>
       </div>
       </main>
       <footer className="footer">
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -4,14 +4,33 @@ import '../hojas-de-estilo/Testimonio.css';
 export function Testimonio(props){
     return (
         <div className= 'contenedor-testimonio'>
+            <div className= 'anyTravel'>
             <img
              className= 'imagen-testimonio'
-            src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+            src={require(`../imagenes/img-${props.imagen}.jpg`)}
             alt= {props.imagen} />
+            </div>
             <div className= 'contenedor-texto-testimonio'>
                 <p className='nombre-testimonio'><strong>{props.nombre} en {props.pais}</strong></p>
                 <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+
                 <p className='texto-testimonio'>"{props.testimonio}"</p>
+                <section className='numerales'>
+                <label for="tentacles">Pasajes:</label>
+                <input type="number" className="contador" id="tentacles" name="tentacles" min="1" max="20" placeholder="1"></input>
+                </section>
+                <section className='fechasTodas'>
+                    <article className='checkIn'>
+                        <label for="start">Check-in:</label>
+                        <input type="date" id="start" name="trip-start"value="2022-05-22" min="2018-01-01" max="2018-12-31" />
+                    </article>
+                    <article className='checkOut'>
+                        <label for="start">Check-Out:</label>
+                        <input type="date" id="start" name="trip-start"value="2022-05-22" min="2022-05-22" max="2024-12-31" />
+                    </article>
+                </section>
+
+                <button className='agregarACarrito'>Agregar a Carrito</button>
             </div>
         </div>
     );
