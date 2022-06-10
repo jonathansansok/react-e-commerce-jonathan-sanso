11d38296ffc218c8207576faec413b39f0d4b141
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-06-09 22:50:15 -0300

    js

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -18,30 +18,6 @@ const ItemCount = () => {
 
 	return (
 		<div className='App'>
-			<Header title={title} subtitle='Soy un subtitulo por props' />
-			<p>User Name: {name}</p>
-
-			<button onClick={handlerUpdateName}>Actualizar Nombre</button>
-			<div className='UserSection'>
-				<CardComponent
-					name='Daniel Di Salvo'
-					date='Se unio en Enero 2021'
-					description='Front-end Developer y profesor del Curso de React.js en CoderHouse'
-					img='https://react.semantic-ui.com/images/avatar/large/matthew.png'
-				/>
-				<CardComponent
-					name='Raul Gutierrez'
-					date='Se unió en Abril 2021'
-					description='Scrum Master y UX Designer en Mercado Libre'
-					img='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
-				/>
-				<CardComponent
-					name='Francesca Bertotti'
-					date='Se unió en Julio 2021'
-					description='DevOps y profesora del curso de React.js en CoderHouse'
-					img='https://react.semantic-ui.com/images/avatar/large/daniel.jpg'
-				/>
-			</div>
 			<div className='CounterSection'>
 				<p>Counter: {counter}</p>
 				<div className='btn-section'>
