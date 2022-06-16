8f126b586db706b956851d54e8fb22bee55c21e3
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-06-16 12:34:19 -0300

    js

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../hojas-de-estilo/Footer.css';
+
+function Footer(){
+    return (
+        <footer className="footer">
+        <p className="creador"><small>Website made by Jonathan Sansó 🖥️</small></p>
+        <ul className="iconostodos">
+            <li className="icon"><a href="https://www.instagram.com/jonathan.sanso/" target="_blank"><img src="https://icongr.am/fontawesome/instagram.svg?size=30&color=901886" alt="instagram" /></a></li>
+            <li className="icon"><a href="https://www.linkedin.com/in/jonathan-sanso-fullstack" target="_blank"><img src="https://icongr.am/fontawesome/linkedin.svg?size=30&color=901886" alt="linked-In" /></a></li>
+            <li className="icon"><a href="https://open.spotify.com/playlist/61gDxDScsKGSxY4wobXCya?si=09b8a654cc744767" target="_blank"><img src="https://icongr.am/fontawesome/spotify.svg?size=30&color=901886" alt="spotify" /></a></li>
+            <li className="icon"><a href="https://www.facebook.com/jonathan.sanso" target="_blank"><img src="https://icongr.am/fontawesome/facebook.svg?size=30&color=901886"  alt="facebook" /></a></li>
+        </ul>
+    </footer>
+        );
+    }
+            
+export default Footer; 
