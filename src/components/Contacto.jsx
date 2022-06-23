10e081e0b183338fca09f87bf185c2e7e5fe1af0
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-06-23 19:14:03 -0300

    js

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
- 
+import '../hojas-de-estilo/contacto.css';
+
+
 const Contacto = () => {
 	return (
-    <div>Mi contacto</div>
+    <div className='contacto-full'>
+        <h1 className='contacto-full-h1'>Contacto</h1>
+        
+    </div>
     )
 };
 
