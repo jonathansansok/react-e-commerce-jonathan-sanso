c9508f6347b17a1302447a6a2184e2293b3a3edc
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-06-27 12:27:57 -0300

    js

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -6,12 +6,15 @@ import { Link } from 'react-router-dom';
 
 const ItemCount = ({stock,initial,onAdd}) => {
 	const [Numero, setNumero] = useState(initial);
+	const [Disponible, setDisponible] = useState(initial);
 	const [onAdder, setonAdder] = useState('');
+	
 
 
 	const sumar = () => {
 		if (Numero < stock) {
 			setNumero(Numero + 1);
+
 		}else{
 			alert('No puedes agregar mas de 10 pasajes');
 		}
@@ -23,26 +26,32 @@ const ItemCount = ({stock,initial,onAdd}) => {
 			alert('No puedes solicitar 0 pasajes');
 		}
 	};
+	const disponible = () => {
+		
+
+	}
+	
+	
 	const resultadoOk = () => {
 		setonAdder({Numero});
 		console.log(setonAdder);
 	};
 
+
 	return (
-	/* 	<Router> */
-				<div className='CounterSection'>
-					<div className='botonera-acciones'>
-					<p className='botonera-pasaje'> Pasajes: </p>
-						<div className='botonera-sumaresta' onClick={restar}> - </div>
-						<p className='botonera-pasaje'>  {Numero}</p>
-						<div className='botonera-sumaresta' onClick={sumar}> + </div>
-					</div>
-					<button className='agregarACarrito-detail' onClick={resultadoOk}>Agregar a carrito</button>
-					{resultadoOk ? 
-						<Link to="/cart"className='agregarACarrito-detail' onClick={onAdd}>Terminar Compra</Link> : false }
-					
-					
-				</div>
+		<div className='CounterSection'>
+			<div className='botonera-acciones'>
+			<p className='botonera-pasaje'> Pasajes: </p>
+				<div className='botonera-sumaresta' onClick={restar}> - </div>
+				<p className='botonera-pasaje'>  {Numero}</p>
+				<div className='botonera-sumaresta' onClick={sumar}> + </div>
+			</div>
+			<div className='botonera-sumaresta'> Stock: {{stock} - {Numero}}</div>
+			<button className='agregarACarrito-detail' onClick={resultadoOk}>Agregar a carrito</button>
+			{resultadoOk ? 
+				<Link to="/cart"className='agregarACarrito-detail' onClick={onAdd}>Terminar Compra</Link> 
+				: false }					
+		</div>
 	);
 };
 
