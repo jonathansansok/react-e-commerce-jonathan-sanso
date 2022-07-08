fffb14bf119c68b99ff0f37f6ee77e28e2364ee2
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-08 13:29:46 -0300

    ¨js¨

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 export const data = [
 	{ id:1, nombre: 'Sky Ocean' , pais: 'Tailandia', imagen: 'tailandia', incluye: 'Pasaje + estadia', descripcion: "Destinado a familiares y jóvenes, playa y recorridos arquitectónicos.", precio: '3050', categoria: 'Asia/Oceania', stock: 14, initial: 0 },
 	{ id:2, nombre: 'Quintana Roo' , pais: 'México', imagen: 'qroo', incluye: 'Pasaje + estadia', descripcion: "7 playas con ciudades en constante movimiento.", precio: '3070', categoria: 'america', stock: 11, initial: 0  },
-	{ id:3, nombre: 'Bariloche' , pais: 'Argentina', imagen: 'bariloche', incluye: 'Pasaje + estadia', descripcion: "Las mejores montañas nevadas de Latinoamerica, ski y snowboarding.", precio: '2800', categoria: 'america', stock: 14, initial: 0 },
+	{ id:3, nombre: 'Bariloche' , pais: 'Argentina', imagen: 'bariloche', incluye: 'Pasaje + estadia', descripcion: "Las mejores montañas nevadas de Latinoamerica, ski y snowboarding.", precio: '3150', categoria: 'america', stock: 14, initial: 0 },
 	{ id:4, nombre: 'Riviera Maya' , pais: 'México', imagen: 'cancun', incluye: 'Pasaje + estadia', descripcion: "Playas tranquilas, noches largas, público variado.", precio: '5000', categoria: 'america', stock: 13, initial: 0  },
 	{ id:5, nombre: 'Villa la Angostura' , pais: 'Argentina', imagen: 'angostura', incluye: 'Pasaje + estadia', descripcion: "Los mejores cerros y montañas, traking y lagos.", precio: '3300', categoria: 'america', stock: 9, initial: 0  },
 	{ id:6, nombre: 'Tierra del Fuego' , pais: 'Argentina', imagen: 'tierradelfuego', incluye: 'Pasaje + estadia', descripcion: "Recorre el fin del mundo sur, a pie y buque.", precio: '3500', categoria: 'america', stock: 11, initial: 0  },
