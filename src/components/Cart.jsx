e3d244ae0b083cb34d0580b7be16f51d70fb6d6d
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-08 11:36:24 -0300

    ¨js¨

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -57,15 +57,15 @@ function Cart() {
     return (
       <Container>
         <h1 className='cart-full-h1'>Detalles</h1>
-          <div className='cartconteiner'>
+          <div className=''>
           {cartList.length !== 0 ? (
             <div className='cartIzquierda'>
-              {cartList.map(producto => <div className='cartItem' key={producto.id} >
-                  <img src={`/imagenes/img-${producto.imagen}.jpg`} alt='' /> <div className='cartDetalle' > <p>{producto.nombre}</p>
+              {cartList.map(producto => <div className='' key={producto.id} >
+                  <img src={`/imagenes/img-${producto.imagen}.jpg`} alt='' /> <div className='' > <p>{producto.nombre}</p>
 
                     <p>Cantidad: {cantidadTotalItem()} - SubTotal ${producto.precio * producto.cantidad}</p>
                     
-                    <p> <button size="sm" onClick={() => eliminarItem(producto.id)} className="btn btn-danger">Eliminar</button>{' '}</p>
+                    <p> <button onClick={() => eliminarItem(producto.id)} className="">Eliminar</button>{' '}</p>
                   </div>
                 </div>
                 )
@@ -73,21 +73,21 @@ function Cart() {
               </div>
           ) : (
             <>
-              <div className='cartconteiner'>
-                <div className='cartIzquierda'>
+              <div className=''>
+                <div className=''>
                   <h2>Su Carrito Esta Vacio</h2>
-                  <Link className="btn btn-warning" to="/" element={<ItemListContainer />}>Volver a tienda</Link>
+                  <Link className="" to="/" element={<ItemListContainer />}>Volver a tienda</Link>
                 </div>
               </div>
             </>
           )}
-          <div className='cartDerecha'>
+          <div className=''>
             <p>Cantidad Total: {totalPasajes()}</p>
             <p>Total ($) {totalAPagar()}</p>
             </div>
  
         </div>
-        <button className="btn btn-danger" onClick={removeCart}>Vaciar Carrito</button>
+        <button className="" onClick={removeCart}>Vaciar Carrito</button>
       </Container>
     )
   }
