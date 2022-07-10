4e6317bf1847ab1862bdc8adb69df62d62017800
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-10 07:18:59 -0300

    ¨js¨

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,4 +1,4 @@
-/* //imports
+ //imports
 import React from "react";
 import Item from "./Item.jsx";
 import "../hojas-de-estilo/Item.css";
@@ -7,12 +7,12 @@ import "../hojas-de-estilo/ItemList.css";
 const ItemList = ({ productos }) => {	
   return (
     <div className="todasLasItem">
-      {productos.map((reco) => (
-        <Item key={reco.id} data={reco} />
-      ))}
+      {productos.map((travel) => {
+        return <Item maker={travel} key={travel.id} />;
+        })}   
     </div>
   );
 };
 
 export default ItemList;
- */
+ 
