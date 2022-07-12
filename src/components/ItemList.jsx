edad5a4884c4093caa04951dc628e98673dba344
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-11 22:19:08 -0300

    js

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,20 +3,15 @@ import React from "react";
 import "../hojas-de-estilo/Item.css";
 import "../hojas-de-estilo/ItemList.css";
 import Item from "./Item.jsx";
-import { Link } from 'react-router-dom';
 
-const ItemList = ({ travels }) => {	
+const ItemList = ({ productos }) => {	
   return (
-    <Link to={`item/${travels.id}`} key={travels.id}>
     <div className="todasLasItem">
-      {travels.map((maker) => {
+      {productos.map((maker) => {
         return <Item travels={maker} key={maker.id} />
-
           })}   
     </div>
-    </Link>
   );
 };
 
 export default ItemList;
-
