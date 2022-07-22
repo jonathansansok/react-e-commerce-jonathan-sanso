f5adc42b591561d886b2b113c4ad9c9e539d5ac1
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-22 17:02:53 -0300

    js

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 import { useCartContext } from './CartContext.jsx'; 
 import '../hojas-de-estilo/Cart.css'; 
 import { Link } from 'react-router-dom';
-import Shop from './Shop.jsx';
-/*  import {shop} from './Shop.jsx';  */
+
+////
+import {db} from '../firebase/firebaseConfig.js'; 
+import '../hojas-de-estilo/shop.css';
+import TextField from '@mui/material/TextField';
+import MessageSuccess from './MessageSuccess.jsx';
+ import  { useState } from 'react'; 
+import { collection, addDoc } from 'firebase/firestore';
+/////
+
+
 function Cart() {
   const {
     cartList,
@@ -15,50 +24,140 @@ function Cart() {
     eliminarItem,
   } = useCartContext()
 
+
+	const generarOrden =  () => {
+		// Nuevo objeto de orders    
+		let items = [];
+		cartList.forEach((item) => items.push({id: item.id, title: item.titulo, price: item.precio, quantity: item.initial}));
+		return items;
+	}
+
+	const initialState = {
+		name: '',
+		phone: '',
+		email: '',
+	};
+	const styles = {
+		containerShop: {
+		textAlign: 'center',
+		paddingTop: 20,
+		},
+	};
+
+	const [buyer, setValues] = useState(initialState);
+	// Este estado está destinado a guardar el id de la compra
+	const [purchaseID, setPurchaseID] = useState('');
+
+	const handleOnChange = (e) => {
+		const { value, name } = e.target;
+		setValues({ ...buyer, [name]: value });
+	};
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		const docRef = await addDoc(collection(db, 'purchases'), {
+			buyer,
+			fecha: new Date(),
+			totalPagar: totalAPagar(),
+			items:  generarOrden(),
+		});
+    
+
+    setPurchaseID(docRef.id);
+		setValues(initialState);
+    
+    
+	};
+  ////////////////////////
+
   return (
     <main className='cart-full'>
       <h1 className='cart-full-h detalles'>Cart-Detalles</h1>
-      <div className=''>
-      {cartList.length !== 0 ? (
-      <>
-        <div className='renderizada-card'>
-          {cartList.map(producto => <div className='renderizada-card-unidad' key={producto.id}>
-            <img className='renderizada-foto' src={producto.imagen} alt='' /> 
-            <section className='renderizada-specs-todas'>
-              <article className='renderizada-specs-palabras'> <p><strong>{producto.titulo}</strong> en <strong>{producto.pais}</strong></p>
-                <div className='renderizada-botonera-item-cart'>
-                  <p>Pasajes: </p>
-                    <button className='botonera-sumaresta' onClick={() => addToCart({ id: producto.id, initial: -1 })} disabled={producto.initial === 1}><p>-</p></button>
-                    <strong className='botonera-sumaresta-numero' >{producto.initial}</strong>
-                    <button className='botonera-sumaresta' onClick={() => addToCart({ id: producto.id, initial: 1 })} disabled={producto.initial === producto.stock}><p>+</p></button>
-                    <p className='subTotal'>SubTotal U$D <strong>{Number(producto.precio) * producto.initial}</strong>.-</p>
-                </div>
-                <button className='agregarACarrito-detail' onClick={() => eliminarItem(producto.id)} >Eliminar</button>{' '}
+        <div className=''>
+          {cartList.length !== 0 ? (
+          <>
+            <div className='renderizada-card'>
+              {cartList.map(producto => <div className='renderizada-card-unidad' key={producto.id}>
+                <img className='renderizada-foto' src={producto.imagen} alt='' /> 
+                <section className='renderizada-specs-todas'>
+                  <article className='renderizada-specs-palabras'> <p><strong>{producto.titulo}</strong> en <strong>{producto.pais}</strong></p>
+                    <div className='renderizada-botonera-item-cart'>
+                      <p>Pasajes: </p>
+                        <button className='botonera-sumaresta' onClick={() => addToCart({ id: producto.id, initial: -1 })} disabled={producto.initial === 1}><p>-</p></button>
+                        <strong className='botonera-sumaresta-numero' >{producto.initial}</strong>
+                        <button className='botonera-sumaresta' onClick={() => addToCart({ id: producto.id, initial: 1 })} disabled={producto.initial === producto.stock}><p>+</p></button>
+                        <p className='subTotal'>SubTotal U$D <strong>{Number(producto.precio) * producto.initial}</strong>.-</p>
+                    </div>
+                    <button className='agregarACarrito-detail' onClick={() => eliminarItem(producto.id)} >Eliminar</button>{' '}
+                  </article>
+                </section>
+              </div>
+              )}
+            </div>
+            <section className=''>
+              <article className='totales-cart'>
+                <button className='agregarACarrito-detail vaciar-carrito' onClick={removeCart}>Vaciar Carrito</button>
+                <p className='totales-cart-item'>Total Pasajes: <strong>{totalPasajes()}</strong></p>
+                <p className='totales-cart-item'>Total U$D <strong>{totalAPagar()}</strong>.-</p>
               </article>
+              <section >
+                {purchaseID ?   
+                  (
+                    <>
+                      {purchaseID && <MessageSuccess purchaseID={purchaseID} />}
+                      <Link  to="/" className='agregarACarrito-detail'> Volver a tienda</Link>
+                    </>
+                  )
+                  :
+                  (
+                    <div style={styles.containerShop}>
+                      <h1>Complete y viaje</h1>
+                      <form className='FormContainer' onSubmit={onSubmit}>
+                        <TextField
+                          placeholder='Name'
+                          style={{ margin: 10, width: 400 }}
+                          name='name'
+                          value={buyer.name}
+                          onChange={handleOnChange}
+                          required
+                        />
+                        <TextField
+                          placeholder='Phone'
+                          style={{ margin: 10, width: 400 }}
+                          name='phone'
+                          value={buyer.phone}
+                          onChange={handleOnChange}
+                          required
+                        />
+                        <TextField
+                          placeholder='City'
+                          style={{ margin: 10, width: 400 }}
+                          name='email'
+                          value={buyer.email}
+                          onChange={handleOnChange}
+                          required
+                        />
+                        <button className='agregarACarrito-detail'>FINALIZAR COMPRA</button>
+                      </form>
+                    </div>
+                  )
+                }
+              </section>
             </section>
-          </div>
+          </>
+          ) 
+          :
+          (
+            <>
+              <section className=''>
+                <article className='carrito-vacio'>
+                  <h2 className='cart-full-h'>Su Carrito Esta Vacio</h2>
+                  <Link  to="/" ><p className='agregarACarrito-detail volver'>Volver a tienda</p></Link>
+                </article>
+              </section>
+            </>
           )}
-        </div>
-        <section className=''>
-          <article className='totales-cart'>
-          <button className='agregarACarrito-detail vaciar-carrito' onClick={removeCart}>Vaciar Carrito</button>
-          <p className='totales-cart-item'>Total Pasajes: <strong>{totalPasajes()}</strong></p>
-          <p className='totales-cart-item'>Total U$D <strong>{totalAPagar()}</strong>.-</p>
-          </article>
-          <Shop/>
-        </section>
-      </>
-      ) : (
-        <>
-          <section className=''>
-            <article className='carrito-vacio'>
-              <h2 className='cart-full-h'>Su Carrito Esta Vacio</h2>
-              <Link  to="/" ><p className='agregarACarrito-detail volver'>Volver a tienda</p></Link>
-            </article>
-          </section>
-        </>
-      )}
-    </div>
+      </div>
     </main>
   )
 }
