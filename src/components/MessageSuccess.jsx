e8174e564e1e0df3d4484fd5a056dc2645bfde70
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-26 09:41:35 -0300

    js

--- src/components/MessageSuccess.jsx
+++ src/components/MessageSuccess.jsx
@@ -3,6 +3,7 @@ import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import '../hojas-de-estilo/MessageSuccess.css';
 
+
 //este componente se monta cuando el comprador termina toda su compra y le muestra su código unico PurchaseID 
 const MessageSuccess = ({ purchaseID }) => {
 	return (
@@ -11,6 +12,7 @@ const MessageSuccess = ({ purchaseID }) => {
 				<Alert severity='success'>
 					Gracias por su compra! Su id de transacción es: {purchaseID} <br/>
 					En instantes le llegará el mail...
+
 				</Alert>
 			</Stack>
 		</div>
