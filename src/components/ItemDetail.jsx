8d80f556afa21554d639de33cbf99395024fbf51
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-07-26 21:56:59 -0300

    js

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -74,32 +74,25 @@ export function ItemDetail({item}){
                             <button className="buttonsDetail"> Terminar mi compra </button>
                             </Link>
                             </>
-
                             ) 
                             :
                             (
-                              <>
-                                {cantidad===0 
-                                    ? 
-                                    <div className='agregar-volver'>   
-                                    {/* //Mas destructuring */}
-                                        <ItemCount stock={stock} initial={initial} onAdd={agregarAlCarrito}/>
-                                        <Link to='/' > <button className='agregarACarrito-detail terminar'> Elegir mas </button> </Link>
-                                    </div>
-                                        : 
-                                    <>      
-                                    <Link to='/'> <button className='agregarACarrito-detail terminar itemdetail'> Elegir mas </button> </Link>                    
-                                    <Link to='/cart'> <button className='agregarACarrito-detail terminar itemdetail'> Ir al Cart </button> </Link>
-                                    </>
-                                    }
-
-                                    </>  
-
-
-
-
+                            <>
+                            {cantidad===0 
+                            ? 
+                            <div className='agregar-volver'>   
+                            {/* //Mas destructuring */}
+                                <ItemCount stock={stock} initial={initial} onAdd={agregarAlCarrito}/>
+                                <Link to='/' > <button className='agregarACarrito-detail terminar'> Elegir mas </button> </Link>
+                            </div>
+                                : 
+                            <>      
+                            <Link to='/'> <button className='agregarACarrito-detail terminar itemdetail'> Elegir mas </button> </Link>                    
+                            <Link to='/cart'> <button className='agregarACarrito-detail terminar itemdetail'> Ir al Cart </button> </Link>
+                            </>
+                            }
+                            </> 
                             )}
-                            {/* Ternario IF que el usuario elija la cantidad y Agregar al carrito, ELSE> volver a inicio o IR al CART */}
                         </div>
                     </div>
                 </div>
